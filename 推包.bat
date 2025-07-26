a62a0218bdf2d66bfb84d207f46d956def484212
lengyibai <1329670984@qq.com>
2025-07-27 01:15:34 +0800

    .

---
+++ 推包.bat
@@ -0,0 +1,30 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+cd /d "E:\桌面\池塘模拟器部署"
+
+echo 执行 git add . ...
+git add .
+
+echo.
+
+echo 执行 git commit
+git commit -m "."
+
+echo.
+
+echo 执行 git pull ...
+git pull
+
+echo.
+
+echo 请按任意键继续执行 git push ...
+pause
+
+echo 执行 git push ...
+git push
+
+echo.
+
+echo 完成！
